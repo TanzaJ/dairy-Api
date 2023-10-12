7f6b87662f3e766fe1f98a6e60585c9e5931b0f4
Vapidx4 <51037467+Vapidx4@users.noreply.github.com>
2023-10-11 21:14:15 -0400

    Update butter_data.sql

--- dairy-api/data/butter_data.sql
+++ dairy-api/data/butter_data.sql
@@ -1,13 +1,13 @@
 -- Insert dummy data entries
 INSERT INTO butter (milk_id, product_name, dp_id, country_id, brand_id, nutritional_value_id)
 VALUES
-    (1, 'Salted Butter', 1, 1, 1, 1),
-    (2, 'Unsalted Butter', 2, 2, 2, 2),
-    (3, 'Whipped Butter', 3, 3, 3, 3),
-    (1, 'Cultured Butter', 4, 4, 4, 4),
-    (2, 'Clarified Butter', 5, 5, 5, 5),
-    (3, 'Irish Butter', 6, 6, 6, 6),
-    (1, 'European Butter', 7, 7, 7, 7),
-    (2, 'Honey Butter', 8, 8, 8, 8),
-    (3, 'Truffle Butter', 9, 9, 9, 9),
-    (1, 'Goat Butter', 10, 10, 10, 10);
+    (1, 'Salted Butter', 21, 1, 1, 21),
+    (2, 'Unsalted Butter', 22, 2, 2, 22),
+    (3, 'Whipped Butter', 23, 3, 3, 23),
+    (1, 'Cultured Butter', 24, 4, 4, 24),
+    (2, 'Clarified Butter', 25, 5, 5, 25),
+    (3, 'Irish Butter', 26, 6, 6, 26),
+    (1, 'European Butter', 27, 7, 7, 27),
+    (2, 'Honey Butter', 28, 8, 8, 28),
+    (3, 'Truffle Butter', 29, 9, 9, 29),
+    (1, 'Goat Butter', 30, 10, 10, 30);
