0d08e9b16088a6b3ce1ab517756693bfc4640bca
Vapidx4 <51037467+Vapidx4@users.noreply.github.com>
2023-10-11 21:27:41 -0400

    Create brand_data.sql

---
+++ dairy-api/data/brand_data.sql
@@ -0,0 +1,9 @@
+-- Insert dummy data entries
+INSERT INTO brand (brand_name, country_id)
+VALUES
+    ('Brand A', 1),
+    ('Brand B', 2),
+    ('Brand C', 3),
+    ('Brand D', 1),
+    ('Brand E', 2),
+    ('Brand F', 3);
