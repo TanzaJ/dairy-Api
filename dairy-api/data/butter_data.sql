54b7f40b6bba39ab7a87a49381481fe7f3a1aeeb
Vapidx4 <51037467+Vapidx4@users.noreply.github.com>
2023-10-11 21:12:53 -0400

    YOU BUTTER NOT

---
+++ dairy-api/data/butter_data.sql
@@ -0,0 +1,13 @@
+-- Insert dummy data entries
+INSERT INTO butter (milk_id, product_name, dp_id, country_id, brand_id, nutritional_value_id)
+VALUES
+    (1, 'Salted Butter', 1, 1, 1, 1),
+    (2, 'Unsalted Butter', 2, 2, 2, 2),
+    (3, 'Whipped Butter', 3, 3, 3, 3),
+    (1, 'Cultured Butter', 4, 4, 4, 4),
+    (2, 'Clarified Butter', 5, 5, 5, 5),
+    (3, 'Irish Butter', 6, 6, 6, 6),
+    (1, 'European Butter', 7, 7, 7, 7),
+    (2, 'Honey Butter', 8, 8, 8, 8),
+    (3, 'Truffle Butter', 9, 9, 9, 9),
+    (1, 'Goat Butter', 10, 10, 10, 10);
