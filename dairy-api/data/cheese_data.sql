37bfd5049818a201e7cfabc632446d4bb2ba5b5a
Vapidx4 <51037467+Vapidx4@users.noreply.github.com>
2023-10-11 20:58:01 -0400

    Create cheese_data.sql

---
+++ dairy-api/data/cheese_data.sql
@@ -0,0 +1,13 @@
+-- Insert dummy data entries
+INSERT INTO cheese (milk_id, product_name, dp_id, country_id, brand_id, nutritional_value_id)
+VALUES
+    (1, 'Cheddar', 1, 1, 1, 1),
+    (2, 'Gouda', 2, 2, 2, 2),
+    (3, 'Brie', 3, 3, 3, 3),
+    (1, 'Manchego', 4, 4, 4, 4),
+    (2, 'Parmesan', 5, 5, 5, 5),
+    (3, 'Camembert', 6, 6, 6, 6),
+    (1, 'Provolone', 7, 7, 7, 7),
+    (2, 'Mozzarella', 8, 8, 8, 8),
+    (3, 'Blue Cheese', 9, 9, 9, 9),
+    (1, 'Feta', 10, 10, 10, 10);
