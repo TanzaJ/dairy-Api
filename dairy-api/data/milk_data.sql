a9f4d327b0ad2a69b5d8d485ff90bc1de805d91d
Vapidx4 <51037467+Vapidx4@users.noreply.github.com>
2023-10-11 21:25:24 -0400

    Create milk_data.sql

---
+++ dairy-api/data/milk_data.sql
@@ -0,0 +1,13 @@
+-- Insert 10 dummy data entries 
+INSERT INTO milk (name, average_cost, place_of_origin, year_created, country_id, brand_id, nutrition_value_id, animal_id)
+VALUES
+    ('Cow Milk', 2.99, 'United States', 2000, 1, 1, 1, 1),
+    ('Goat Milk', 3.49, 'United States', 2005, 1, 2, 2, 2),
+    ('Sheep Milk', 3.29, 'United States', 1995, 1, 3, 3, 3),
+    ('Organic Cow Milk', 3.99, 'United States', 2010, 1, 4, 4, 1),
+    ('Cow Milk', 2.79, 'Canada', 1990, 2, 5, 5, 1),
+    ('Goat Milk', 2.99, 'Canada', 2002, 2, 6, 6, 2),
+    ('Sheep Milk', 4.19, 'France', 2015, 3, 7, 7, 3),
+    ('Ox Milk', 2.99, 'United States', 2012, 1, 8, 8, 4),
+    ('Buffalo Milk', 2.79, 'United States', 2007, 1, 9, 9, 5),
+    ('Camel Milk', 2.99, 'Thailand', 2014, 4, 10, 10, 6);
