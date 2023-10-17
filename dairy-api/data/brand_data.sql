920d7297a34e878b03ff27538e479f302072ace0
laronichill <laronichill@gmail.com>
2023-10-17 00:29:37 -0400

    Records adding

--- dairy-api/data/brand_data.sql
+++ dairy-api/data/brand_data.sql
@@ -1,9 +1,17 @@
 -- Insert dummy data entries
-INSERT INTO brand (brand_name, country_id)
-VALUES
-    ('Brand A', 1),
-    ('Brand B', 2),
-    ('Brand C', 3),
-    ('Brand D', 1),
-    ('Brand E', 2),
-    ('Brand F', 3);
+INSERT INTO brand (brand_id, brand_name, country_id) VALUES
+(1, 'Kerrygold', 372),
+(2, 'Lactantia', 124),
+(3, 'Dairyland', 124),
+(4, 'Natrel', 124),
+(5, 'Babybel', 250),
+(6, 'Black Diamond', 124),
+(7, 'Kraft', 840),
+(8, 'Cracker Barrel', 840),
+(9, 'Saputo', 124),
+(10, 'Lactalis', 250),
+(11, 'Beatrice Foods', 124),
+(12, 'Chapman', 840),
+(13, 'La Diperie', 124),
+(14, 'Bilboquet', 124),
+(15, 'Laura Secord', 124);
